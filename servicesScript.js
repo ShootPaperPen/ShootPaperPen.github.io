6ec5809f2dae2d9cadb8b1af71f2e4df38bb8d8d
jbernards728 <joaquimnathaniel.bernardo.cics@ust.edu.ph>
2024-12-10 22:38:05 +0800

    Added button animations, Added prices and descriptions, changed some item names

--- servicesScript.js
+++ servicesScript.js
@@ -25,7 +25,7 @@ promoImg.setAttribute("src", "images/" + promoImgFileName);
 
 // For Order Button
 const orderButton = document.getElementById("order");
-const orderURL = "https://www.youtube.com"; // ENTER ORDER URL
+const orderURL = "https://web.facebook.com/profile.php?id=61555627856739"; // ENTER ORDER URL
 orderButton.setAttribute("href", orderURL);
 
 // Use to hold objects
@@ -152,50 +152,49 @@ function createNewItem(itemName, price, tags, desc) {
 
 createNewItem(
   "2B - Nier Automata",
-  645000.9,
+  300,
   "Female, Games",
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in orci at elit ultricies hendrerit. Donec pulvinar ex justo, ac sagittis massa fringilla ut. Morbi accumsan consectetur vehicula. Proin vitae ligula neque. Aliquam laoreet, augue non ornare lacinia, dolor metus rutrum mi, sed posuere ex leo sed lectus. Phasellus fermentum, libero a elementum pharetra, velit lorem porta justo, eget malesuada urna nibh nec ipsum. Morbi malesuada risus turpis, ut volutpat turpis efficitur ut. Maecenas elit diam, pellentesque vel porta non, tincidunt quis sapien. Integer ac ornare tortor, et tempor turpis. Suspendisse posuere lobortis velit quis rhoncus. Donec ut lacinia quam. Phasellus sit amet lorem ligula. Phasellus arcu nulla, condimentum vel commodo sit amet, convallis tincidunt enim. Quisque vulputate magna sem, quis tempus odio aliquam eu. Etiam iaculis lacinia nunc in tincidunt."
-);
-createNewItem("Aura - Freiren Beyond Journey's End", 645000.9, "Female, Anime");
-createNewItem("Autumn Fox", 645000.9, "Female, Misc");
-createNewItem("Battle Bunny Miss Fortune - LoL", 645000.9, "Female, Games");
-createNewItem("Black Lobelia - My Dress-Up Darling", 645000.9, "Female, Anime");
-createNewItem("Bomb Devil - Chainsaw Man", 645000.9, "Female, Anime");
-createNewItem("CC - Code Geass", 645000.9, "Female, Anime");
-createNewItem("Cleo de Nile - Monster High", 645000.9, "Female");
-createNewItem("Dolia - Honor of Kings", 645000.9, "Female, Games");
-createNewItem("Fern - Freiren Beyond Journey's End", 645000.9, "Female, Anime");
-createNewItem("Fiery Spirit Diaochan - Honor of Kings", 645000.9, "Female, Games");
+  "300 cos, wig + 200 virtuous treaty sword + 200 boots/heels");
+createNewItem("Aura - Freiren Beyond Journey's End", 500, "Female, Anime", "500 cos, wig + 200 balance prop + 200 boots");
+createNewItem("Autumn Kitsune", 500, "Female, Misc", "500 chinese hanfu only + 200 other cos parts & wig + 100 umbrella & fan");
+createNewItem("Battle Bunny Miss Fortune - LoL", 800, "Female, Games", "800 cos, styled wig + 400 guns + 300 boots ");
+createNewItem("Black Lobelia - My Dress-Up Darling", 400, "Female, Anime", "400 cos, wig, accessories + 200 boots");
+createNewItem("Bomb Devil - Chainsaw Man", 500, "Female, Anime", "500 bomb devil head, apron, cos + 200 boots + 200 reze default");
+createNewItem("CC - Code Geass", 500, "Female, Anime", "500 cos, wig + 200 boots");
+createNewItem("Cleo de Nile - Monster High", 500, "Female", "500 cos, wig + 200 sandal heels");
+createNewItem("Dolia - Honor of Kings", 500, "Female, Games", "500 cos, styled wig + 150 bag + 100 mermaid ears + 150 sandals");
+createNewItem("Fern - Freiren Beyond Journey's End", 400, "Female, Anime", "400 cos, wig + 200 staff prop + 200 boots");
+createNewItem("Fiery Spirit Diaochan - Honor of Kings", 800, "Female, Games", "800 cos, wig + 200 heels + 200 veil fans");
 createNewItem(
   "Freiren - Freiren Beyond Journey's End",
-  645000.9,
-  "Female, Anime"
+  350,
+  "Female, Anime", "350 cos, wig + 200 staff prop + 200 boots"
 );
-createNewItem("Gwen - LoL", 645000.9, "Female, Games");
-createNewItem("Howl - Howl's Moving Castle", 645000.9, "Male, Anime");
-createNewItem("Howl(Blonde) - Howl's Moving Castle", 645000.9, "Male, Anime");
-createNewItem("Jinx - Arcane", 645000.9, "Female, Games");
-createNewItem("Kafka - HSR", 1000.4, "Female, Games");
-createNewItem("Kafka(Dress ver.) - HSR", 645000.9, "Female, Games");
-createNewItem("Lute - Hazbin Hotel", 645000.9, "Female, Misc");
-createNewItem("Mai Sakurajima - Bunny Girl Senpai", 645000.9, "Female");
-createNewItem("Maomao - Apothecary Diaries", 645000.9, "Female, Anime");
-createNewItem("Nanami Kento - Jujutsu Kaisen", 645000.9, "Male, Anime");
-createNewItem("Nazuna Nanakusa - Call of the Night", 645000.9, "Female, Anime");
+createNewItem("Gwen - LoL", 600, "Female, Games", "600 cos, miku wig (shorter)\n800 cos, gwen wig (longer) + 100 gwen heels (DIY heels) + 250 white maryjane shoes + 400 scissors ");
+createNewItem("Howl Black & Bird - Howl's Moving Castle", 400, "Male, Anime", "400 cos, wig, accessories + 400 wings ");
+createNewItem("Howl(Blonde) - Howl's Moving Castle", 400, "Male, Anime", "400 cos, wig, accessories");
+createNewItem("Jinx - Arcane", 500, "Female, Games", "500 cos, unstyled wig (neoprene top)\n700 cos, unstyled wig (leather top) + 300 upgrade to styled wig + 200 boots + 200 zapper & flamechompers + 100 goggles + 500 fishbones");
+createNewItem("Kafka - HSR", 400, "Female, Games", "400 cos, wig + 200 boots + 400 gun props");
+createNewItem("Kafka(Dress ver.) - HSR", 600, "Female, Games", "600 cos, wig + 200 heels");
+createNewItem("Lute - Hazbin Hotel", 300, "Female, Misc", "300 cos, wig, halo + 200 wings + 100 sword + 200 helmet + 200 boots");
+createNewItem("Mai Sakurajima - Bunny Girl Senpai", 200, "Female", "300 cos, mai wig + 200 heels\n250 bunny suit + 200 and above if with different character wig ");
+createNewItem("Maomao - Apothecary Diaries", 600, "Female, Anime", "600 cos, wig + 200 footwear");
+createNewItem("Nanami Kento - Jujutsu Kaisen", 400, "Male, Anime", "400 cos, wig, accessories + 100 knife prop");
+createNewItem("Nazuna Nanakusa - Call of the Night", 300, "Female, Anime", "300 cos, wig, accessories + 200 boots");
 createNewItem(
   "Raphtalia - Rising of the Shield Hero",
-  645000.9,
-  "Female, Anime"
+  400,
+  "Female, Anime", "400 cos, wig, accessories + 200 boots + 200 sword"
 );
-createNewItem("Red Blood Cell - Cells at Work", 645000.9, "Female, Anime");
-createNewItem("Reina - Tekken 8", 645000.9, "Female, Games");
-createNewItem("Reze - Chainsaw Man", 645000.9, "Female, Anime");
-createNewItem("Rizu Kyun - My Dress-Up Darling", 645000.9, "Female, Anime");
-createNewItem("Sage - Valorant", 645000.9, "Female, Games");
-createNewItem("Seraphine - LoL", 645000.9, "Female, Games");
-createNewItem("Spirit Blossom Ahri - Lol", 645000.9, "Female, Games");
-createNewItem("Ubel - Freiren Beyond Journey's End", 645000.9, "Female, Anime");
-createNewItem("Xayah - LoL", 645000.9, "Female, Games");
-createNewItem("Yuta Okkotsu - Jujutsu Kaisen", 645000.9, "Male, Anime");
-createNewItem("Zero Two - Darling in the Franxx", 23.3, "Female, Anime");
-createNewItem("Zero Two(Maid) - Darling in the Franxx", 99.75, "Female, Anime");
+createNewItem("Red Blood Cell - Cells at Work", 200, "Female, Anime", "200 cos, wig, accessories ");
+createNewItem("Reina - Tekken 8", 400, "Female, Games", "400 cos, styled wig + 200 boots");
+createNewItem("Reze - Chainsaw Man", 300, "Female, Anime", "300 cos, wig,  +  200 boots/heels");
+createNewItem("Rizu Kyun - My Dress-Up Darling", 400, "Female, Anime", "400 cos, wig, accessories + 200 boots/heels");
+createNewItem("Sage - Valorant", 400, "Female, Games", "400 cos, wig + 200 boots");
+createNewItem("Seraphine - LoL", 900, "Female, Games", "900 cos, styled wig + 100 wings + 200 mic + 200 boots/heels");
+createNewItem("Spirit Blossom Ahri - Lol", 450, "Female, Games", "450 cos, unstyled wig, ears+ 50 upgraded ears, + 500 ninetails, + 200 single tail + 100 geta + 200 orb");
+createNewItem("Ubel - Freiren Beyond Journey's End", 400, "Female, Anime", "400 cos, wig + 200 staff prop + 200 boots");
+createNewItem("Xayah - LoL", 900, "Female, Games", "900 cos, styled wig");
+createNewItem("Yuta Okkotsu - Jujutsu Kaisen", 500, "Male, Anime", "500 cos, wig, accessories + 200 sword");
+createNewItem("Zero Two - Darling in the Franxx", 400, "Female, Anime", "400 cos, wig, accessories + 200 coat/hat/gloves + 200 boots");
+createNewItem("Zero Two(Maid) - Darling in the Franxx", 300, "Female, Anime", "300 cos, wig, accessories + 200 heels + 50 petticoat");
